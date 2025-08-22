7322a626df77e77ffbf45d271dd1244e71b00b38
PraveenKumar05-hub <praveenkkris@gmail.com>
2025-08-22 16:14:18 +0530

    Array

---
+++ arraysearch.java
@@ -0,0 +1,29 @@
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.*;
+public class arraysearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size:");
+        int n = sc.nextInt();
+        int flag = 0;
+        int[] arr = new int[n];
+        System.out.print("Elements are:");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter Serach Element:");
+        int search = sc.nextInt();
+        for(int i=0;i<n;i++){
+            if(search==arr[i]){
+                flag = search;
+                break;
+            }
+        }
+        if(flag==search){
+            System.out.print("Element Found");
+        }
+        else{
+            System.out.print("Element not Found");
+        }
+    }
+}
